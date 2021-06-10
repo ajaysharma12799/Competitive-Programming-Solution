fb5cc104389927a4baa2211a9b404c5a2f1b3430
ajaysharma12799 <ajaysharma12799@gmail.com>
2021-06-10 15:55:32 +0530

    Removed Unnecessary Code

--- 1. Easy/automorphic_number.js
+++
@@ -1,8 +0,0 @@
-function checkAutomorphicNumber (value) {
-    let lastRemainder = value % 10, automorphic = 0, automorphicRemainder = 0;
-    automorphic = Math.pow(value, 2);
-    automorphicRemainder = automorphic % 10;
-    return lastRemainder == automorphicRemainder ? true : false;
-}
-
-checkAutomorphicNumber(5) ? console.log('Automorphic Number') : console.log('Not Automorphic Number');
--- 2. Medium/maximun_number_of_handshake.js
+++
@@ -1,6 +0,0 @@
-function noOfHandshake (value) {
-    let handshake = ( value * (value - 1) ) /2
-    console.log(handshake);
-}
-
-noOfHandshake(2);
